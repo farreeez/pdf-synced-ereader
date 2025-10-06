3e422cafac687b887f2c7ab1da477f20e63e0835
farreeez <fares.selwadii@gmail.com>
2025-10-06 14:12:31 +1300

    added styling to the front page when there are no books

--- synced-ereader-frontend/src/pages/Home.jsx
+++ synced-ereader-frontend/src/pages/Home.jsx
@@ -7,19 +7,24 @@ export default function Home() {
   return (
     <div className="HomeContainer">
       <div className="HomeHeader">
-        <div id="HomeHeaderTitle">Audiobook Synced Ereader</div>
+        <div className="title">Audiobook Synced Ereader</div>
         <button className="HomeButton">+ New Book</button>
       </div>
       <div className="BooksContainer">
-        {books.length != 0 ? (
+        {!books || books.length != 0 ? (
           <div className="BooksGrid">
             <p className="test">books are here</p>
             <p className="test">books are here</p>
           </div>
         ) : (
           <div className="BooksGrid">
-            <p className="test">no books are here</p>
-            <p className="test">no books are here</p>
+            <svg></svg>
+            <div className="title">No Books Yet</div>
+            <div className="NoBooksDescription">
+              Add your first book to start syncing PDFs with audiobooks for a
+              better reading experience.
+            </div>
+            <button className="HomeButton">+ Add Your First Book</button>
           </div>
         )}
       </div>
